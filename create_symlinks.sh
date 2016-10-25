ffb8a3663f27aa2daf2d7111d55c34e562566aaf
Keno Fischer <keno@juliacomputing.com>
2016-10-25 17:03:00 -0400

    Setup MTA

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,2 +1,5 @@
 sudo ln -s /home/keno/anubis.juliacomputing.io/sysctl/60-anubis.conf /etc/sysctl.d/60-anubis.conf
 sudo ln -s /home/keno/anubis.juliacomputing.io/pam.d/chsh /etc/pam.d/chsh
+sudo ln -s /home/keno/anubis.juliacomputing.io/postfix/main.cf /etc/postfix/main.cf
+sudo ln -s /home/keno/anubis.juliacomputing.io/opendkim.conf /etc/opendkim.conf
+sudo ln -s /home/keno/anubis.juliacomputing.io/default/opendkim /etc/default/opendkim

d1f05f56dff25e78070a4e9069d3b692a0dd636c
Keno Fischer <keno@juliacomputing.com>
2017-01-22 21:38:13 -0500

    Make slightly more general

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,5 +1,7 @@
-sudo ln -s /home/keno/anubis.juliacomputing.io/sysctl/60-anubis.conf /etc/sysctl.d/60-anubis.conf
-sudo ln -s /home/keno/anubis.juliacomputing.io/pam.d/chsh /etc/pam.d/chsh
-sudo ln -s /home/keno/anubis.juliacomputing.io/postfix/main.cf /etc/postfix/main.cf
-sudo ln -s /home/keno/anubis.juliacomputing.io/opendkim.conf /etc/opendkim.conf
-sudo ln -s /home/keno/anubis.juliacomputing.io/default/opendkim /etc/default/opendkim
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p /etc/postfix
+sudo ln -s $DIR/sysctl/60-debugging.conf /etc/sysctl.d/60-debugging.conf
+sudo ln -s $DIR/pam.d/chsh /etc/pam.d/chsh
+sudo ln -s $DIR/postfix/main.cf /etc/postfix/main.cf
+sudo ln -s $DIR/opendkim.conf /etc/opendkim.conf
+sudo ln -s $DIR/default/opendkim /etc/default/opendkim
